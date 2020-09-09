5d13e2cdb9e1cc09afc5640aa1edf7eb3d6d9e5f
mtfuji3776 <32.allegro.con.brio.ed.appassionato@gmail.com>
2020-09-09 18:16:06 +0900

    Applicativeまで定義した

--- src/Lens_State.hs
+++ src/Lens_State.hs
@@ -13,6 +13,20 @@ instance Functor Counter where
     fmap f (C  (St p)) = C . St $ (cross id f) . p
 
 -- 対角射
+diagonal :: a -> (a,a)
 diagonal x = (x,x)
 -- 積の普遍射
+produniv :: (a1 -> b1) -> (a1 -> b2) -> a1 -> (b1,b2)
 produniv f g = cross f g . diagonal
+-- 評価射
+eval :: (a,a->b) -> b
+eval = uncurry (flip ($))
+
+instance Applicative Counter where
+    pure x = C $ St (\s -> (s,x))
+    (C (St f)) <*> (C (St x)) = let p1 = view _1 . f
+                                    p2 = view _2 . f
+                                    t1 = view _1 . x
+                                    t2 = view _2 . x
+                                in C $ St (produniv (t1 . p1) (eval . produnive (t2 . p1) p2))
+
